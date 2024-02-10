8f300598a9a08dd559e26054c4a97ca40c4e032f
KunAttila69 <71005372+KunAttila69@users.noreply.github.com>
2024-02-10 20:44:41 +0100

    Add files via upload

--- FelveteliWeb/diak.js
+++ FelveteliWeb/diak.js
@@ -1,7 +1,10 @@
 class Diak{
-    constructor(omAz,nev,matek,magyar){
+    constructor(omAz,nev,email,szulDat,ertCim,matek,magyar){
         this.omAz = omAz;
         this.nev = nev;
+        this.email = email;
+        this.szulDat = szulDat;
+        this.ertCim = ertCim;
         this.matek = matek;
         this.magyar = magyar;
         this.osszes = matek+magyar;
@@ -9,6 +12,7 @@ class Diak{
 
     ConvertToRow = () => {
         let tr = document.createElement("tr")
+        tr.ondblclick = () => {this.CopyData()}
         let data = [this.omAz,this.nev,this.matek,this.magyar,this.osszes]
         data.forEach((value)=>{
             let td = document.createElement("td")
@@ -20,12 +24,7 @@ class Diak{
 
     ConvertToData = () => {
         let data = document.createElement("div");
-        data.classList.add("student-data");
-        
-        let btn = document.createElement("button");
-        btn.id="edit-button"
-        btn.innerText = "✎";
-        data.appendChild(btn);
+        data.classList.add("student-data"); 
         
         let head = document.createElement("div");
         head.classList.add("data-head");
@@ -43,24 +42,28 @@ class Diak{
         
         let scoreContainer = document.createElement("div");
         scoreContainer.classList.add("score-container");
-        
-        let mathScore = document.createElement("h3")
-        mathScore.innerText = this.matek;
-        let plusSign = document.createElement("h3")
-        plusSign.innerText = "+";
-        let litScore = document.createElement("h3")
-        litScore.innerText = this.magyar;
-        
-        scoreContainer.appendChild(mathScore);
-        scoreContainer.appendChild(plusSign);
-        scoreContainer.appendChild(litScore);
+        let scoreText = document.createElement("h3")        
+        scoreText.innerText = `Matematika: ${this.matek} + Magyar: ${this.magyar}`
+
+        scoreContainer.appendChild(scoreText); 
         
         data.appendChild(scoreContainer);
         
         let score = document.createElement("h2");
-        score.innerText = this.osszes;
+        score.innerText = `Összes pontszám: ${this.osszes}`;
         data.appendChild(score);
         
         return data
     }
+
+    ConvertToCSV = () => {
+        return `${this.omAz};${this.nev};${this.email};${this.szulDat};${this.ertCim};${this.matek};${this.magyar}`
+    }
+
+    CopyData = async () => {
+        await navigator.clipboard.writeText(this.ConvertToCSV());
+
+        alert(`${this.omAz} azonsítójú diák CSV formátumban vágólapra mentve!`);
+    }
 }
+
--- FelveteliWeb/main.js
+++ FelveteliWeb/main.js
@@ -1,9 +1,9 @@
 const data = [
-    new Diak("98657574765", "Gipsz János", 50, 1),
-    new Diak("12345678901", "Kovács Eszter", 43, 3),
-    new Diak("98765432109", "Nagy Gábor", 2, 44),
-    new Diak("55555555555", "Varga Anna", 13, 5),
-    new Diak("77777777777", "Kis Péter", 3, 22),
+    new Diak("98657574765", "Gipsz János","janos@gmail.com","1997-06-14","Hódmezővásárhely", 50, 1),
+    new Diak("12345678901", "Kovács Eszter","eszter@gmail.com", "1997-06-14","Hódmezővásárhely", 43, 3),
+    new Diak("98765432109", "Nagy Gábor","gabor@gmail.com", "1997-06-14","Hódmezővásárhely", 2, 44),
+    new Diak("55555555555", "Varga Anna","anna@gmail.com", "1997-06-14","Hódmezővásárhely", 13, 5),
+    new Diak("77777777777", "Kis Péter","peter@gmail.com", "1997-06-14","Hódmezővásárhely", 3, 22),
 ]
 
 const pgNotFound = document.getElementById("id-not-found-page")
@@ -12,7 +12,7 @@ const pgUserAdd = document.getElementById("create-student-page")
 
 let order = {
     base: "nev",
-    desc: true
+    desc: false
 }
 
 let limit = 0
@@ -35,6 +35,7 @@ const SetOrder = (val) => {
     element.innerHTML += order.desc ? "▼" : "▲"
     LoadData()
 }
+
 
 const OrderData = () =>{
     data.sort((a, b) => {
@@ -95,8 +96,7 @@ const LoadData = () => {
     })
 }
 
-const SearchUser = () => {
-    
+const SearchUser = () => { 
     const searchInput = document.getElementById("searchInput").value
     pgUserAdd.style.display = "none"
     pgNotFound.style.display = "none"
@@ -114,19 +114,47 @@ const SearchUser = () => {
 const HandleInput = () => { 
     const options = document.getElementById("ids")
     options.innerHTML = ""
-    let search = document.getElementById("searchInput").value;
-    search = search.slice(0, 11);  
-    
+    let searchInput = document.getElementById("searchInput");
+    let search = searchInput.value;
+
+    if (search.length > 11) {
+        search = search.slice(0, 11);
+        searchInput.value = search;
+    }
     let ids = []
+    let mathAvgVal = 0
+    let litAvgVal = 0
+    let sumAvgVal = 0
     data.forEach((item)=>{
         if(search === item.omAz.slice(0,search.length)){
             ids.push(item.omAz)
+            mathAvgVal += item.matek
+            litAvgVal += item.magyar
+            sumAvgVal += item.osszes
         }
     })
+    resCount.innerText = ids.length
+    mathAvg.innerText = ids.length > 0 ? (mathAvgVal / ids.length).toFixed(1) : 0
+    litAvg.innerText = ids.length > 0 ? (litAvgVal / ids.length).toFixed(1) : 0
+    sumAvg.innerText = ids.length > 0 ? (sumAvgVal / ids.length).toFixed(1) : 0
     ids.forEach((id)=>{
         let option = document.createElement("option")
         option.innerText = id
         options.appendChild(option)
     })
 }
- 
+
+const ExportData = async () => {
+    csvData = "OM_azonosito;Nev;Email;Szuletesi_datum;Ertesitesi_cim;matek_eredmeny;magyar_eredmeny"
+    data.forEach((line)=>{
+        csvData += `${line.ConvertToCSV()}\n`
+    })
+
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvData);
+    hiddenElement.target = '_blank';  
+    hiddenElement.download = 'Diakok.csv';  
+    hiddenElement.click();   
+}
+HandleInput()
+SetOrder("nev")
